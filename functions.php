ae1271660dbc4e88c9be2572c76ccf69a7f80ce1
Joachim Happel <joachim.happel@gmail.com>
2016-03-14 18:44:58 +0100

    Frontpage Hooks
    Bewirkt, dass Klicks auf die Module der Frontpage direkt zu den Diensten führen.

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+/* Write your awesome functions below */
+add_action( '__grid_single_post_content' , function(){
+	global $post;
+	
+	$url = get_post_meta ($post->ID, 'link_zum_dienst', true);
+	
+	echo '<i style="display:none" class="link_zum_dienst">'.$url.'</i>';
+	
+});
+add_action( 'wp_footer', function(){
+	?><script>
+	jQuery(document).ready(function($){
+		$('i.link_zum_dienst').each(function(i, el){
+	  
+		  var p = $(el).parents('article')[0];
+			  
+		  $(p).find('a.tc-grid-bg-link').attr('href',$(el).html()) ;
+		  $(p).find('h2.entry-title a').first().attr('href',$(el).html()) ;
+
+		});
+	});
+	
+	</script><?php
+});
