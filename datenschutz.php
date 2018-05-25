3db2f3cb22e0dfc1d7d4b12e02697816a9d480ae
Frank Staude <frank@staude.net>
2018-05-25 15:28:28 +0200

    datenschutz template

---
+++ datenschutz.php
@@ -0,0 +1,71 @@
+<?php
+
+
+
+
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<![endif]-->
+
+</head>
+
+<body <?php body_class(); ?>>
+
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			// Start the loop.
+			while ( have_posts() ) :
+				the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+					<?php
+					wp_link_pages(
+						array(
+							'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfifteen' ) . '</span>',
+							'after'       => '</div>',
+							'link_before' => '<span>',
+							'link_after'  => '</span>',
+							'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>%',
+							'separator'   => '<span class="screen-reader-text">, </span>',
+						)
+					);
+					?>
+				</div><!-- .entry-content -->
+
+				<?php edit_post_link( __( 'Edit', 'twentyfifteen' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer><!-- .entry-footer -->' ); ?>
+
+				</article><!-- #post-## -->
+
+<?php
+				// End the loop.
+			endwhile;
+			?>
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+</body>
+</html>
